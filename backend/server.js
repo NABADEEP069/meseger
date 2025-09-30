87788bf24e82367590aed907c9282f4dd8ee074e
NABADEEP KR. DAS <nabadeepdas069@gmail.com>
2025-09-30 23:48:55 +0530

    srt be 1

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,21 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
+
+
+
 app.get("/", (req,res) => {
     res.send("API IS RUNNING");
 })
-app.listen(5000, console.log("Server is running "));
+
+
+app.get("/v1/chats", (req,res) => {
+    res.send(chats);
+})
+
+app.get("v1/chats/:id", (req, res) => {
+    console.log(req.params.id);
+    const singleChat = chats.find((c) => c.id === req.params.id);
+    res.send(singleChat); 
+})
+app.listen(5000, console.log("Server is running smoothly"));
